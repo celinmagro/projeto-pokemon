1686ca0563cd3475405d707a35eb5b60edd89ca9
celin <79673244+celinmagro@users.noreply.github.com>
2023-05-14 14:07:50 -0300

    implement dark-mode

--- src/js/index.js
+++ src/js/index.js
@@ -1,26 +1,17 @@
 // Objetivo: Alterar tema.
 
-// N1 - Clique no botão de troca de tema, adicionar classe dark-mode no body.
 const buttonChangeTheme = document.getElementById("change-theme");
-
-// N2 - Pegar no JS o elemento HTML correspondente ao body.
 const body = document.querySelector("body");
-
-// Buscando img
 const imageButtonChangeTheme = document.querySelector(".image-button");
 
-// N3 - Identificar o clique do usuário no botão change-theme.
 buttonChangeTheme.addEventListener("click",() => {
-    // N4 - Adicionar classe dark-mode no body.
-    body.classList.add("dark-mode");
+    const darkModeActivated = body.classList.contains("dark-mode");
 
-    //N5 - Alterar imagem do botão.
-    imageButtonChangeTheme.setAttribute("src", "./src/images/moon.png")
+    body.classList.toggle("dark-mode");
 
-    // Objetivo 2: Voltar o tema para o modo claro.
-
-    // N6 - Remover "dark-mode" no body.
-    if (body.classList.contains("dark-mode")) {
-        
+    if (darkModeActivated) {
+        imageButtonChangeTheme.setAttribute("src", "./src/images/sun.png");
+    } else {
+        imageButtonChangeTheme.setAttribute("src", "./src/images/moon.png");
     }
 });
